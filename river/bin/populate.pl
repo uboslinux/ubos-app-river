7768f45e4ee2da83ecdfb35f5552ab086cae1fdd
Johannes Ernst <jernst@indiecomputing.com>
2018-04-02 21:54:39 +0000

    Change directories Cleaning up PKGBUILDs

--- river/bin/populate.pl
+++ river/bin/populate.pl
@@ -13,7 +13,7 @@ use UBOS::Utils;
 if( 'install' eq $operation ) {
     my $dataDir     = $config->getResolve( 'appconfig.datadir' );
     my $listsDir    = "$dataDir/lists";
-    my $listsSrcDir = '/usr/share/river/lists';
+    my $listsSrcDir = '/ubos/share/river/lists';
 
     if( UBOS::Utils::isDirEmpty( $listsDir ) && !UBOS::Utils::isDirEmpty( $listsSrcDir )) {
         UBOS::Utils::myexec( "cp -d -r -p --reflink=auto $listsSrcDir/* $listsDir/" );
